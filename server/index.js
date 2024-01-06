26bb4ecbb0628ea8b475f55edab8f30b045846ae
박현우 <ghwhsbsb123@gmail.com>
2024-01-06 18:23:55 +0900

    server.js

--- server/index.js
+++ server/index.js
@@ -21,7 +21,6 @@ client.connect(); // 데이터베이스에 접속함
 app.post('/register', async (req, res) => {
   try {
     const { user_id, user_pw, name, call, birth } = req.body;
-
     const hashedPassword = await bcrypt.hash(user_pw, 10);
     const randomToken = uid(24);
 
@@ -32,11 +31,11 @@ app.post('/register', async (req, res) => {
 
     res.status(200).json({ success: true, user: result.rows[0] });
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    res.status(500).json({ success: false, error: error.message });
   }
 });
 
-app.post('/kakaoregister', async (req, res) => {
+app.post('/register/kakao', async (req, res) => {
   try {
     const { kakao_id, name, call, birth } = req.body;
     const randomToken = uid(24);
@@ -45,9 +44,10 @@ app.post('/kakaoregister', async (req, res) => {
       'INSERT INTO users (kakao_id, name, call, birth, token) VALUES ($1, $2, $3, $4, $5) RETURNING *',
       [kakao_id, name, call, birth, randomToken]
     );
+
     res.status(200).json({ success: true, user: result.rows[0] });
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    res.status(500).json({ success: false, error: error.message });
   }
 });
 
@@ -55,57 +55,73 @@ app.post('/login', async (req, res) => {
   try {
     const { user_id, user_pw, kakao_id } = req.body;
 
-    if ((user_id && user_pw) || kakao_id) {
-      let result;
+    if (user_id && user_pw) {
+      //일반 로그인
+      const result = await client.query(
+        'SELECT * FROM users WHERE user_id = $1',
+        [user_id]
+      );
 
-      if (user_id && user_pw) {
-        //일반 로그인
-        result = await client.query('SELECT * FROM users WHERE user_id = $1', [
-          user_id,
-        ]);
-
-        if (result.rows.length == 0) {
-          return res
-            .status(401)
-            .json({ success: false, message: 'Invalid user_id or password' });
-        }
-
-        const isPasswordValid = await bcrypt.compare(
-          user_pw,
-          result.rows[0].user_pw
-        );
-
-        if (!isPasswordValid) {
-          return res
-            .status(401)
-            .json({ success: false, message: 'Invalid user_id or password' });
-        }
-      } else {
-        //카카오 로그인
-
-        result = await client.query('SELECT * FROM users WHERE kakao_id = $1', [
-          kakao_id,
-        ]);
-
-        if (result.rows.length == 0) {
-          return res
-            .status(401)
-            .json({ sucess: false, message: 'Invalid kakao_id' });
-        }
+      if (result.rows.length == 0) {
+        return res
+          .status(401)
+          .json({ success: false, error: '존재하지 않는 계정입니다.' });
       }
 
-      const token = generateToken(result.rows[0].user_id);
+      const isPasswordValid = await bcrypt.compare(
+        user_pw,
+        result.rows[0].user_pw
+      );
 
-      return res
-        .status(200)
-        .json({ success: true, user: result.rows[0] }, token);
-    } else {
-      return res
-        .status(400)
-        .json({ success: false, message: 'Invalid id and pw' });
+      if (!isPasswordValid) {
+        return res
+          .status(401)
+          .json({ success: false, error: '비밀번호가 일치하지 않습니다.' });
+      }
+
+      return res.status(200).json({ success: true, user: result.rows[0] });
     }
+
+    if (kakao_id) {
+      //카카오 로그인
+      const result = await client.query(
+        'SELECT * FROM users WHERE kakao_id = $1',
+        [kakao_id]
+      );
+
+      if (result.rows.length == 0) {
+        return res
+          .status(401)
+          .json({ sucess: false, error: '존재하지 않는 계정입니다.' });
+      }
+
+      return res.status(200).json({ success: true, user: result.rows[0] });
+    }
+
+    res.status(500).json({ success: false });
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    res.status(500).json({ success: false, error: error.message });
+  }
+});
+
+app.post('/verify', async (req, res) => {
+  try {
+    const { token } = req.body;
+
+    const result = await client.query(
+      'SELECT * FROM users WHERE user_id = $1',
+      [token]
+    );
+
+    if (result.rows.length == 0) {
+      return res
+        .status(401)
+        .json({ success: false, error: '잘못된 토큰입니다.' });
+    }
+
+    res.status(200).json({ success: true, user: result.rows[0] });
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message });
   }
 });
 
